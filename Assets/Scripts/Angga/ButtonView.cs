7885509abd48ce853922c08310497f66d070594b
erlanggasbr <sembiringerlangga@gmail.com>
2025-07-02 10:59:28 +0700

    Modify script
    Modif script buat nyobain vcontainer

---
+++ Assets/Scripts/Angga/ButtonView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SotongStudio
+{
+    public class ButtonView : MonoBehaviour
+    {
+        public Button SeedSelectedButton;
+        public Button FightButton;
+        public Button UseSeedButton;
+        public Button ThrowSeedButton;
+    }
+}
--- Assets/Scripts/Angga/TurnBasedInstaller.cs
+++ Assets/Scripts/Angga/TurnBasedInstaller.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using VContainer;
+using VContainer.Unity;
 
 namespace SotongStudio
 {
     public class TurnBasedInstaller : ScopeInstallHelper
     {
+        [SerializeField]
+        private ButtonView _buttonView;
+        [SerializeField]
+        private BattleSystemView _battleSystemView;
+
         public override void Install(IContainerBuilder builder)
         {
-            builder.Register<BattleSystemController>(Lifetime.Singleton);
+            //builder.Register<BattleSystemControl>(Lifetime.Singleton);
+            builder.RegisterEntryPoint<BattleSystemLogic>();
+            builder.RegisterComponent(_buttonView);
+            builder.RegisterComponent(_battleSystemView);
         }
     }
 }
