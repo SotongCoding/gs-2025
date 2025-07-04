d5912eb83c32326670e9c7ba162c520be4b9b212
erlanggasbr <sembiringerlangga@gmail.com>
2025-07-04 22:20:15 +0700

    audio manager
    first commit

---
+++ Assets/Scripts/Audio Service/AudioService.cs
@@ -0,0 +1,32 @@
+using NaughtyAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SotongStudio
+{
+    public class AudioService : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _musicSource;
+        [SerializeField] private AudioSource _sfxSource;
+
+        [SerializeField] private List<AudioClip> _musicClips;
+        [SerializeField] private List<AudioClip> _sfxClips;
+
+        public void PlayMusic(int index)
+        {
+            _musicSource.clip = _musicClips[index];
+            _musicSource.loop = true;
+            _musicSource.Play();
+        }
+
+        public void PlaySFX(int index)
+        {
+            _sfxSource.PlayOneShot(_sfxClips[index]);
+        }
+
+        public void StopMusic()
+        {
+            _musicSource.Stop();
+        }
+    }
+}
